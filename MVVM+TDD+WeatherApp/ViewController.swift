6070d682920aa89bf6ea371aa879f3191145eb50
barteklanczyk <miltenkot@gmail.com>
2020-09-11 19:02:22 +0200

    Initial Commit

---
+++ MVVM+TDD+WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM+TDD+WeatherApp
+//
+//  Created by Bartek Lanczyk on 11/09/2020.
+//  Copyright © 2020 Bartek Lanczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
